e0deedf159541fe06bfb51a4fe6b42aecdec597c
Ziad <ziad.safari1@gmail.com>
2022-10-12 11:46:54 -0400

    testpush2

--- firstSite/home/views.py
+++ firstSite/home/views.py
@@ -4,5 +4,5 @@ import datetime
 # Create your views here.
 def current_datetime(request):
     now = datetime.datetime.now()
-    html = "<html><body>It is now %s. </body></html>" % now
+    html = "<html><body>It is now %s.  </body></html>" % now
     return HttpResponse(html)
