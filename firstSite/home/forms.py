8c58540e6ebdd94710d404dc26fee5847ed410a6
Ziad <ziad.safari1@gmail.com>
2022-10-13 12:39:40 -0400

    most recent website layout

--- firstSite/home/forms.py
+++ firstSite/home/forms.py
@@ -1,7 +1,22 @@
 from django.forms import ModelForm
 from .models import Input
+from django.forms import TextInput, NumberInput
 # creating a form
 class InputForm(ModelForm):
      class Meta:
          model = Input
          fields = '__all__'
+         widgets = {
+            'quantity': NumberInput(attrs={
+                'class': 'form-control',
+                'style': "min-width: 400px",
+                'placeholder': "0",
+            }),
+            'unit': TextInput(attrs={
+                'class': 'form-control',
+            }),
+            'name': TextInput(attrs={
+                'class': 'form-control',
+                "placeholder": "Food Type",
+            }),
+         }
