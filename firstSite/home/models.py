e8dfd1c2afb486166a15e2bd6cc3a2ccc01cdd5e
YummyTaco <chickentaco823@gmail.com>
2022-10-13 15:38:42 -0400

    Add in Date Object and modify views.home

--- firstSite/home/models.py
+++ firstSite/home/models.py
@@ -1,8 +1,26 @@
+from contextlib import nullcontext
 from django.db import models
+from django.db.models import IntegerField, Model
+from django.core.validators import MaxValueValidator, MinValueValidator
+
 UNIT_CHOICES = (
     ('grams','Grams'),
     ('lbs','Pounds'),
     ('kilograms','Kilograms')
+)
+MONTH_CHOICES = (
+    ('January'),
+    ('February'),
+    ('March'),
+    ('April'),
+    ('May'),
+    ('June'),
+    ('July'),
+    ('August'),
+    ('September'),
+    ('October'),
+    ('November'),
+    ('December')
 )
 # Create your models here.
 class Input(models.Model):
@@ -18,7 +36,20 @@ class AddFood(models.Model):
     def __str__(self):
 	    return self.name
 
+class Day(models.Model):
+    # day = models.IntegerField(validators=[MaxValueValidator(30),MinValueValidator(1)](null=True))
+    # month = models.CharField(choices=MONTH_CHOICES, default='January',null=True)
+    # year = models.IntegerField(null=True)
+    day = models.CharField(null=True)
+    month = models.CharField(null=True)
+    year = models.IntegerField(null=True)
+    
+    def __str__(self):
+        return self.month + "/" + str(self.day) + "/" + str(self.year)
+
+
 class Food(models.Model):
+    day = models.ForeignKey(Day, null=True, on_delete=models.SET_NULL)
     sugar_g = models.IntegerField(null=True)
     fiber_g = models.IntegerField(null=True)
     serving_size_g = models.IntegerField(null=True)
