99371f76a23160972e786d9675f49aadfb884d1f
Ziad <ziad.safari1@gmail.com>
2022-10-13 13:05:03 -0400

    dark mode changes

--- firstSite/home/forms.py
+++ firstSite/home/forms.py
@@ -1,14 +1,9 @@
 from django.forms import ModelForm
-<<<<<<< HEAD
 from .models import Input
 from django.forms import TextInput, NumberInput
-=======
-from .models import *
->>>>>>> 70bdf6c406dbda9778aaa63610dd632cff488360
 # creating a form
 class foodForm(ModelForm):
      class Meta:
-<<<<<<< HEAD
          model = Input
          fields = '__all__'
          widgets = {
@@ -25,12 +20,3 @@ class foodForm(ModelForm):
                 "placeholder": "Food Type",
             }),
          }
-=======
-         model = AddFood
-         fields = '__all__'
-
-class attrForm(ModelForm):
-    class Meta:
-        model = Food
-        fields = '__all__'
->>>>>>> 70bdf6c406dbda9778aaa63610dd632cff488360
